1ee8c9f56dfa8827a4f6da06566c433f94b992fc
Whoger <gonzalo.toscano.h@gmail.com>
2018-05-14 12:55:11 +0200

    Claaaaaaaaaaaaaaaarix

--- src/java/servlets/SeleccionarUser.java
+++ src/java/servlets/SeleccionarUser.java
@@ -21,72 +21,72 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 public class SeleccionarUser extends HttpServlet {
-    @EJB QuidditchEJB miEJB;
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        List<String> empleados = miEJB.selectAllNombreusuario();
-        request.setAttribute("empleados", empleados);
-        
-        if ("Borrar usuario".equals(request.getParameter("borrarUsuario"))) {
-            request.getRequestDispatcher("/borrarUsuario.jsp").forward(request,response);
-        }
-        else if ("Modificar contra".equals(request.getParameter("cambiarContra"))) {
-            request.getRequestDispatcher("/cambiarContra.jsp").forward(request,response);
-        }
-        else if ("Crear una incidencia".equals(request.getParameter("insertarIncidencia"))) {
-            request.getRequestDispatcher("/insertarIncidencia.jsp").forward(request,response);
-        }
-        
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+//    @EJB QuidditchEJB miEJB;
+//
+//    /**
+//     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+//     * methods.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        
+//        List<String> empleados = miEJB.selectAllNombreusuario();
+//        request.setAttribute("empleados", empleados);
+//        
+//        if ("Borrar usuario".equals(request.getParameter("borrarUsuario"))) {
+//            request.getRequestDispatcher("/borrarUsuario.jsp").forward(request,response);
+//        }
+//        else if ("Modificar contra".equals(request.getParameter("cambiarContra"))) {
+//            request.getRequestDispatcher("/cambiarContra.jsp").forward(request,response);
+//        }
+//        else if ("Crear una incidencia".equals(request.getParameter("insertarIncidencia"))) {
+//            request.getRequestDispatcher("/insertarIncidencia.jsp").forward(request,response);
+//        }
+//        
+//    }
+//
+//    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+//    /**
+//     * Handles the HTTP <code>GET</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Handles the HTTP <code>POST</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Returns a short description of the servlet.
+//     *
+//     * @return a String containing servlet description
+//     */
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }// </editor-fold>
 
 }
